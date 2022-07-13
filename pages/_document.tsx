f42a7b0ddad77a651080253935b2ec5a1b6ad80d
Emmanuel <opensoftweb@gmail.com>
2022-07-13 15:08:01 +0100

    fix nextjs styled-component css not loading

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,26 @@ import Document, {
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
   render() {
     return (
       <Html>
@@ -27,29 +47,29 @@ export default class MyDocument extends Document {
   }
 }
 
-MyDocument.getInitialProps = async (ctx: DocumentContext) => {
-  const sheet = new ServerStyleSheet();
-  const originalRenderPage = ctx.renderPage;
+// MyDocument.getInitialProps =  async (ctx: DocumentContext) => {
+//   const sheet = new ServerStyleSheet();
+//   const originalRenderPage = ctx.renderPage;
 
-  try {
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-      });
+//   try {
+//     ctx.renderPage = () =>
+//       originalRenderPage({
+//         enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+//       });
 
-    const initialProps: DocumentInitialProps = await Document.getInitialProps(
-      ctx
-    );
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          {sheet.getStyleElement()}
-        </>
-      ),
-    };
-  } finally {
-    sheet.seal();
-  }
-};
+//     const initialProps: DocumentInitialProps = await Document.getInitialProps(
+//       ctx
+//     );
+//     return {
+//       ...initialProps,
+//       styles: (
+//         <>
+//           {initialProps.styles}
+//           {sheet.getStyleElement()}
+//         </>
+//       ),
+//     };
+//   } finally {
+//     sheet.seal();
+//   }
+// };
